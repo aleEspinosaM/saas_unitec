b7bbb96184b1d14d8f581d52704265c4b01fa069
Ale Espinosa <alex.espinosadelosmonteros@gmail.com>
2016-06-07 04:49:23 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @basic_plan = Plan.find(1) #query string parameters que asociacmos en la base de datos
+    @pro_plan = Plan.find(2)
   end
   
   def about
