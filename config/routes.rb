f6548624fccda33e4aa41e9c79970a60c24455bb
Ale Espinosa <alex.espinosadelosmonteros@gmail.com>
2016-06-08 18:55:34 +0000

    ya salio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :contacts #para ver todas las rutas bundle exec rake routes
+  resources :users do
+    resource :profile
+  end
+  resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
