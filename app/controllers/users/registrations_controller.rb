934906ad444fd3265045689a69e60919bc07ead8
Ale Espinosa <alex.espinosadelosmonteros@gmail.com>
2016-06-08 05:46:20 +0000

    Added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    
+  
+  before_filter :select_plan, only: :new
+
+
+
   def create
-    super do |resource| #importing the creat action from devise
-      if params[:plan] #estamos checando si hay algun from con planes
-        resource.plan_id = params[:plan] #si es verdadero lo guardamos en resource.planid que es una columna en la tabla users
-        if resource.plan_id == 2 #si el id es 2 significa que es pro account y lo guardamos
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
           resource.save_with_payment
         else
           resource.save
         end
       end
+    end
+  end
+  
+  private
+  def select_plan
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Porfavor selecciona un plan de membresia."
+      redirect_to root_url
     end
   end
   
