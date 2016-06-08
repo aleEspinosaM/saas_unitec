d6dc8b0f4fc10501b318b31532f144265f7e6715
Ale Espinosa <alex.espinosadelosmonteros@gmail.com>
2016-06-08 04:57:49 +0000

    Updated routes file with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts #para ver todas las rutas bundle exec rake routes
   get '/about' => 'pages#about'
   root 'pages#home'
